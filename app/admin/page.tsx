0e001ededa3fc3f48325c639995ac5f242b8e691
Anshit Kabariya <jackofficial388@gmail.com>
2025-02-24 23:03:28 +0530

    Add AdminRedirect component for automatic redirection to admin site and update dependencies

---
+++ app/admin/page.tsx
@@ -0,0 +1,18 @@
+ "use client"
+ 
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+const AdminRedirect = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Redirect to your admin website URL
+    // router.push('https://rising-admin.vercel.app/')
+    router.push(`${process.env.REACT_FRONT_URL}`)
+  }, [router])
+
+  return null // Or you can show a loading spinner here if you want
+}
+
+export default AdminRedirect
